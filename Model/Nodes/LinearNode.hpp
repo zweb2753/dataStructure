d84ccc12f8acd1dca736d0a62561ab4c8bbc10e8
zweb2753 <31513803+zweb2753@users.noreply.github.com>
2018-02-13 08:08:30 -0700

    save

---
+++ Model/Nodes/LinearNode.hpp
@@ -0,0 +1,21 @@
+//
+//  LinearNode.hpp
+//  dataStructure
+//
+//  Created by Weber, Zane on 2/12/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef LinearNode_hpp
+#define LinearNode_hpp
+
+#include <stdio.h>
+
+#endif /* LinearNode_hpp */
+template <class Type>
+class LinearNode();
+LinearNode(Type data);
+LinearNode(Type data, LinearNode)
+
+template <classType>
+void LinkedList<Type> :: add
