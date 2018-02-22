ff78abc43c787ee065af54772e7fd4ab275896b1
zweb2753 <31513803+zweb2753@users.noreply.github.com>
2018-02-22 09:23:17 -0700

    added queue class and added methods to it

--- Model/LinearStructures/Queue.hpp
+++ Model/LinearStructures/Queue.hpp
@@ -9,6 +9,137 @@
 #ifndef Queue_hpp
 #define Queue_hpp
 
-#include <stdio.h>
+#include "LinkedList.hpp"
+
+#include <iostream>
+using namespace std;
 
 #endif /* Queue_hpp */
+
+
+template <class Type>
+class Queue : public LinkedList<Type>
+{
+public:
+    Queue();
+    ~Queue();
+    
+    void enqueue(type data);
+    Type dequeue();
+    Type peek();
+    void clear();
+    
+    void add(Type data);
+    void addAtIndex(int index, Type data);
+    Type getFromIndex(int index);
+    Type remove(int index);
+}
+ template <class Type>
+Queue<Type> :: Queue() : LinkeList<Type>()
+{
+    
+}
+
+template <class Type>
+Queue<Type> :: ~Queue()
+{
+    for(LinearNode<Type> * removed = this->front; removed != nullptr; removed = this->front)
+    {
+        this->front = removed->getNextNode();
+        delete removed;
+    }
+}
+
+template <class Type>
+void Queue<type> * added = new LinearNode<Type>(item);
+{
+    LinearNode<Type> * added = new LinearNode<Type>(item);
+
+    if(this->size == 0)
+    {
+        this->front = added;
+    }
+    else
+    {
+        this->end->setNextNode(added);
+    }
+
+    this->end = added;
+    this->size++;
+}
+
+template <class Type>
+void Queue<Type> :: addAtIndex(int index, Type item)
+{
+    assert(index == this->size - 1);
+    enqueue(item);
+}
+
+template <class Type>
+void Queue<Type> :: add(Type item)
+{
+    enqueue(item);
+}
+
+template <class Type>
+Type Queue<Type> :: dequeue()
+{
+    assert(this->size > 0);
+    
+    Type returned = this->front->getData();
+    
+    LinearNode<Type> * removed = this->front;
+    this->front = removed->getNextNode();
+    
+    delete removed;
+    
+    this->size = this->size -1;
+    
+    return returned;
+}
+
+template <class Type>
+Type Queue<Type> :: remove(int index)
+{
+    assert(index == 0);
+    return dequeue();
+}
+
+template <class Type>
+void Queue<Type> :: clear()
+{
+    while(this->front != nullptr)
+    {
+        cout << dequeue() << endl;
+    }
+}
+
+template <class Type>
+Type Queue<Type> :: peek()
+{
+    assert(this->size > 0);
+    return this->getFront()->getData();
+}
+
+template <class Type>
+Type Queue<Type> :: peek()
+{
+    assert(this->size > 0);
+    return this->getFront()->getData();
+}
+
+template <class Type>
+Type Queue<Type> :: getFromINdex(int index)
+{
+    assert(index == 0);
+    return peek();
+}
+
+
+
+
+
+
+
+
+
