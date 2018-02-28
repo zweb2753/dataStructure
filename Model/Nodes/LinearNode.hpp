0240edab8fc245ef2a2b61a0b81e22ebc4654345
zweb2753 <31513803+zweb2753@users.noreply.github.com>
2018-02-28 08:32:11 -0700

    finished LinearNode

--- Model/Nodes/LinearNode.hpp
+++ Model/Nodes/LinearNode.hpp
@@ -9,13 +9,49 @@
 #ifndef LinearNode_hpp
 #define LinearNode_hpp
 
-#include <stdio.h>
+#include "Node.hpp"
 
 #endif /* LinearNode_hpp */
 template <class Type>
-class LinearNode();
-LinearNode(Type data);
-LinearNode(Type data, LinearNode)
+class LinearNode : public Node<Type>
+{
+protected:
+    LinearNode<Type> * next;
+public:
+    LinearNode();
+    LinearNode(Type data);
+    LinearNode(Type data, LinearNode<Type> * next);
+    
+    void setNextNode(LinearNode<Type> * next);
+    LinearNode<Type> * getNextNode();
+};
 
-template <classType>
-void LinkedList<Type> :: add
+template <class Type>
+LinearNode<Type> :: LinearNode() : Node<Type>()
+{
+    this->next = nullptr;
+}
+
+template <class Type>
+LinearNode<Type> :: LinearNode(Type data) : Node<Type>(data)
+{
+    this->next = nullptr;
+}
+
+template <class Type>
+LinearNode<Type> :: LinearNode(Type data, LinearNode<Type> * next) : Node<Type>(data)
+{
+    this->next = next;
+}
+
+template <class Type>
+void LinearNode<Type> :: setNextNode(LinearNode<Type> * nextNodePointer )
+{
+    this->next = nextNodePointer;
+}
+
+template <class Type>
+LinearNode<Type> * LinearNode<Type> :: getNextNode()
+{
+    return next;
+}
