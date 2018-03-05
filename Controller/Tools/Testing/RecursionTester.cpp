bb994d7edc1eac6de57ac55ef96f7ef8b3ef6121
zweb2753 <31513803+zweb2753@users.noreply.github.com>
2018-03-05 08:02:35 -0700

    added to the .cpp file

--- Controller/Tools/Testing/RecursionTester.cpp
+++ Controller/Tools/Testing/RecursionTester.cpp
@@ -7,3 +7,33 @@
 //
 
 #include "RecursionTester.hpp"
+
+
+int RecursionTester :: fib(int number)
+{
+    if ( number == 0 || number == 1 )
+    {
+        cout << "reached a base case" << endl;
+        return 1;
+    }
+    else
+    {
+        cout << "calling fib with: " << number - 1 << " and " << number - 2 << endl;
+        return fib (number - 1) + fib (number - 2);
+    }
+}
+
+string RecursionTester :: mystery(string word)
+{
+    return "";
+}
+
+void RecursionTester :: testRecursionNumbers()
+{
+    
+}
+
+void RecursionTester :: testRecursionString()
+{
+    
+}
