6621c3604bd2098db0ba9051fa3049b5bfdc9e67
zweb2753 <31513803+zweb2753@users.noreply.github.com>
2018-03-19 08:47:38 -0600

    Added graph.hpp header file

---
+++ Model/Graph.cpp
@@ -0,0 +1,9 @@
+//
+//  Graph.cpp
+//  dataStructure
+//
+//  Created by Weber, Zane on 3/19/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#include "Graph.hpp"
---
+++ Model/Graph.hpp
@@ -0,0 +1,50 @@
+//
+//  Graph.hpp
+//  dataStructure
+//
+//  Created by Weber, Zane on 3/19/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+#ifndef Graph_hpp
+#define Graph_hpp
+
+#include <stdio.h>
+#include <set>
+#include <queue>
+
+using namespace std;
+template <class Type>
+class Graph
+{
+private:
+    static const int MAXIMUM = 20;
+    bool adjacencyMatrix [MAXIMUM][MAXIMUM];
+    int weightCostMatrix [MAXIMUM][MAXIMUM];
+    Type graphData[MAXIMUM];
+    int vertexCount;
+    void depthFirstTraversal(Graph<Type> & graph, int vertex, bool markedVertices[]);
+public:
+    Graph();
+    
+    void addVertex(const Type& value);
+    
+    void addEdge(int source, int target);
+    void addEdgeUndirected(int source, int target);
+    void addEdgeCost(int source, int target);
+    
+    Type& operator [] (int vertex);
+    Type operator [] (int vertex) const;
+    int size() const;
+    
+    bool hasUndirectedConnection(int source, int target) const;
+    bool areConnected(int source, int target)const;
+    std::set<int> neighbors(int vertex) const;
+    
+    void depthFirstTraversal(Graph<Type> & graph, int vertex);
+    void breadthFirstTraversal(Graph<Type> & graph, int vertex);
+    int costTraversal(Graph<Type> & graph, int vertex);
+};
+
+
+#endif /* Graph_hpp */
